53d9e1baed041e19b6cfa158b43dd01fa07ea4a3
Kevin07k <88062400+Kevin07k@users.noreply.github.com>
2022-12-22 20:13:48 -0400

    Problema Kitchen Timings

---
+++ CodeChef/Division4/Kitchen Timings/Kitchen Timings.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int N,X,Y;
+    cin>>N;
+    int Resp[N];
+    for (int i = 0; i < N; ++i) {
+        cin>>X>>Y;
+        Resp[i]=X-Y;
+    }
+    for (int i = 0; i < N; ++i) {
+        cout<<Resp[i]<<endl;
+    }
+}
---
+++ CodeForce/CodeForcesRound#839(Div3)/ProblemaA/cmake-build-debug/CMakeFiles/ProblemaA.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProblemaA.dir/A+B.cpp.obj"
+  "CMakeFiles/ProblemaA.dir/A+B.cpp.obj.d"
+  "ProblemaA.exe"
+  "ProblemaA.exe.manifest"
+  "ProblemaA.pdb"
+  "libProblemaA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProblemaA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
